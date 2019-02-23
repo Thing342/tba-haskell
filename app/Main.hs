e35e827b15c0c48ea7c9d3368fa9fa16f674f207
Wes <wesjordansemailaddress@gmail.com>
2019-02-23 13:12:57 -0500

    Add more demos

--- app/Main.hs
+++ app/Main.hs
@@ -10,18 +10,21 @@ import TBA.Match
 import TBA.Games.DeepSpace
 
 import Control.Lens
-import Network.Wreq (responseBody)
+import Data.Aeson (Array)
+import Data.Aeson.Lens (key, _Integer)
+import Network.Wreq (responseBody, Response)
 
 import qualified Data.Text as T
+import qualified Data.Set as S
 
-main :: IO ()
-main = do
-    putStrLn "Enter auth code: "
-    auth <- getLine
-    client <- tbaNew $ T.pack auth
+main :: IO()
+main = showTeamAttrition
+
+showAverageScores :: IO ()
+showAverageScores = do
+    client <- createClient
+    event <- ask "Enter code of event to get scores: "
     
-    putStrLn "Enter code of event to get scores: "
-    event <- getLine
     matches <- tbaEventMatches event client
 
     let scores = matches ^.. responseBody . traverse . (redScore <> blueScore) . totalPoints
@@ -30,3 +33,28 @@ main = do
     putStrLn $ "Scores: " ++ (show scores)
     putStrLn $ "Total: " ++  (show $ sum scoresF)
     putStrLn $ "Average: " ++ (show $ mean scoresF)
+
+showTeamAttrition :: IO ()
+showTeamAttrition = do
+    client <- createClient
+    d1Name <- ask "Enter old district code: (e.g. 2016fim)"
+    d2Name <- ask "Enter new district code: (e.g. 2017fim)"
+
+    d1Set <- teamNumSet d1Name client
+    d2Set <- teamNumSet d2Name client
+
+    let attrition = d1Set `S.difference` d2Set
+    let rookies = d2Set `S.difference` d1Set
+
+    putStrLn $ "Attrition: " ++ (show attrition)
+    putStrLn $ "Rookies: " ++ (show rookies)
+
+ask :: String -> IO String
+ask question = do
+    putStrLn question
+    getLine
+
+createClient :: IO TBAClient
+createClient = do
+    auth <- ask "Enter auth code: "
+    tbaNew $ T.pack auth
--- src/TBA.hs
+++ src/TBA.hs
@@ -10,11 +10,12 @@ import Network.Wreq
 import Control.Lens
 import Data.Aeson
 import Data.Aeson.Types
-import Data.Aeson.Lens (_String, _Double, AsValue, key, values, nth)
+import Data.Aeson.Lens (_String, _Double, _Integer, AsValue, key, values, nth)
 import Data.Scientific
 
 import qualified Network.Wreq.Session as S
 import qualified Data.Text as T
+import qualified Data.Set as DS
 
 data TBAClient = TBAClient {
     tbaAuth :: T.Text
@@ -43,3 +44,10 @@ tbaRequest path client = do
 
 tbaEventMatches :: String -> TBAClient -> IO (Response [Match])
 tbaEventMatches eventCode = tbaRequest ("event/" ++ eventCode ++ "/matches")
+
+teamNumSet :: String -> TBAClient -> IO (DS.Set Integer)
+teamNumSet dName client = do
+    let path = "district/" ++ dName ++ "/teams"
+    dTeamsJson <- tbaRequest path client :: IO (Response Array)
+    let dSet = DS.fromList $ dTeamsJson ^.. responseBody . traverse . key "team_number" . _Integer
+    return dSet
